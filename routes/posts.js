86ca4fade3efed3daacf6448653abe614b78b22b
Aditya Sharma <aditya07543@gmail.com>
2021-11-16 09:30:18 +0530

    DOuble authentication for post

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts_controller');
+const passport = require('passport');
 
-router.post('/create', postsController.createPost);
+router.post(
+  '/create',
+  passport.checkAuthentication,
+  postsController.createPost
+);
 module.exports = router;
