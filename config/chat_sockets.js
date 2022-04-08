71355d26345d247a3f665b20118b1b8e28f32325
Aditya Sharma <aditya07543@gmail.com>
2022-04-08 09:06:24 +0530

    removing console.log

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -4,20 +4,20 @@ module.exports.chatSockets = function (socketServer) {
   const io = new Server(socketServer, { cors: { origin: '*' } });
 
   io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+    // console.log('a user connected', socket.id);
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      // console.log('user disconnected');
     });
 
     socket.on('join_room', function (data) {
-      console.log('Joining request received', data);
+      // console.log('Joining request received', data);
 
       socket.join(data.chatroom);
 
       io.in(data.chatroom).emit('user_joined', data);
     });
     socket.on('send_message', function (data) {
-      console.log('aaya', data);
+      // console.log('aaya', data);
       io.in(data.chatroom).emit('receive_message', data);
     });
   });
--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (env.name == 'development') {
   );
 }
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended : false}));
 
 app.use(cookieParser());
 app.use(expressLayouts);
